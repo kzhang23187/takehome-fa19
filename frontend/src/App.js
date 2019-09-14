2068403bda4fa78199aa023d459bd70da83c985c
Kevin Zhang <kzhang23187@gmail.com>
2019-09-14 15:18:43 -0500

    Finished part 5 of front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,37 +11,41 @@ class App extends Component {
         {id: 1, name: "Angad", nickname: "greg", hobby: "dirty-ing"},
         {id: 2, name: "Roy", nickname: "uwu", hobby: "weeb"},
         {id: 3, name: "Daniel", nickname: "oppa", hobby: "losing money with options trading"},
-      ]
+      ],
+      value: ''
     }
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.changeText = this.changeText.bind(this);
   }
   handleSubmit(event) {
-
+    this.refs.counter.incrementClick()
+    this.setState({
+      contacts: this.state.contacts.concat({id: this.state.contacts.length + 1, name: this.state.value, nickname: "nickname", hobby: "hobby"})
+    })
   }
+  changeText(event){
+        this.setState(
+            {value : event.target.value}
+        );
+    }
   render() {
     return (
       <div className="App">
         <Instructions complete={true}/>
-        <Counter />
+        <Counter ref = "counter"/>
         <h2>Add New Contact</h2>
         <form onSubmit={this.handleSubmit}>
           <input
-            value='Name'
+            value={this.state.value}
+            onChange = {this.changeText}
             type='text'
             name='Name'/>
-          <input
-            value='Nickname'
-            type='text'
-            name='Nickname'/>
-          <input
-            value='Hobby'
-            type='text'
-            name='Hobby'/>
         </form>
+        <button onClick = {this.handleSubmit}>Submit</button>
         <br></br>
         <h2>Existing Contacts</h2>
         {this.state.contacts.map(x => (
-          <Contact id={x.id} name={x.name} nickcname={x.nickname} hobby={x.hobby} />
+          <Contact id={x.id} name={x.name} nickname={x.nickname} hobby={x.hobby} />
         ))}
       </div>
     )
